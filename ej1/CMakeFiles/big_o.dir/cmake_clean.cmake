1eddeadd9ce891f9bfcb7503669c6716c1fddfc2
kasios <princekasios@gmail.com>
2013-10-24 20:37:03 +0200

    Compilable a medias

---
+++ ej1/CMakeFiles/big_o.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/big_o.dir/big_o.cpp.o"
+  "big_o.pdb"
+  "big_o"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/big_o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
